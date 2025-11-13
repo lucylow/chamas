ff2a680a38f28649b618adde62da70bd23cbfa56
づ｡◕‿◕｡)づ lucylow <llow@edu.uwaterloo.ca>
2025-11-13 16:35:07 -0500

    .

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { wagmiConfig } from './lib/wagmiConfig';
